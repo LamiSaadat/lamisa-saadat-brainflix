9ad9d073264663f34807c5e85b837127c3848b1a
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-06-11 16:19:50 -0400

    Remove console logs and make app.js a functional component. Remove files not required.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import "./App.scss";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import React, { Component } from "react";
 import Upload from "./pages/Upload";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/upload" component={Upload} />
-          <Route path="/video/:id" component={Home} />
-        </Switch>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/upload" component={Upload} />
+        <Route path="/video/:id" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
